df61e928b5b9a21506b57547998678bbf0b03542
ktsk1 <108092000+ktsk1@users.noreply.github.com>
2023-02-14 14:33:38 +0300

    Update movie_picker.py

--- movie_picker/movie_picker.py
+++ movie_picker/movie_picker.py
@@ -1,11 +1,13 @@
 import random
 
 select = input("This is a random movie picker. ----- FOR RANDOM -> TYPE 1 , FOR RANDOM BY FIRST LETTER -> TYPE 2 ")
+
 if select == "1":
     randomize = print("Picking a movie...")
     with open("movies.txt") as f:
         lines = f.readlines()
         print (random.choice(lines))
+        
 elif select == "2":
     letter = input("Choose a letter: ")
     with open("movies.txt") as f:
